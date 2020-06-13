14ccaf6c6b7f9ba856ae20351b87e155ae2cd749
skps2010 <skps2010030908@yahoo.com>
2020-06-13 17:37:20 +0800

    add gameround & invokeEvent packet

--- Assets/Script/GameChoice.cs
+++ Assets/Script/GameChoice.cs
@@ -9,4 +9,9 @@ public class GameChoice
     public static GameMode GameMode;
     public static string ServerName;
     public static string Winner;
+
+    public static bool IsServer()
+    {
+        return GameMode == GameMode.Server;
+    }
 }
